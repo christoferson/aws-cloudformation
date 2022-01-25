2217987fe53cadc76cf0d51bf33c5478dabd0e01
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-01-25 22:51:14 +0900

    ses-lambda Add ExpireDate and Cleanup

--- ses/ses-lambda-dynamo/ses-lambda-dynamo.js
+++ ses/ses-lambda-dynamo/ses-lambda-dynamo.js
@@ -5,16 +5,19 @@ exports.handler = function (event, context, callback) {
   console.log("Received event:", JSON.stringify(event, null, 2));
 
   var SnsPublishTime = event.Records[0].Sns.Timestamp;
-  var SnsTopicArn = event.Records[0].Sns.TopicArn;
+  //var SnsTopicArn = event.Records[0].Sns.TopicArn;
   var SESMessage = event.Records[0].Sns.Message;
 
   SESMessage = JSON.parse(SESMessage);
   console.log("SES Message: " + JSON.stringify(SESMessage, null, 2));
 
-  var SesMessageType = SESMessage.eventType; //SESMessage.notificationType;
+  const SesMessageType = SESMessage.eventType; //SESMessage.notificationType;
   var SesMessageId = SESMessage.mail.messageId;
   var SesDestinationAddress = SESMessage.mail.destination.toString();
-  var LambdaReceiveTime = new Date().toString();
+  //var LambdaReceiveTime = new Date().toString();
+  let ExpireDate = new Date();
+  const TTL_DELTA = 60 * 60 * 24 * 90; // Keep records for 90 days
+  let ExpireEpoch = (Math.floor(ExpireDate.valueOf() / 1000) + TTL_DELTA).toString();
   
   console.log("Got MessageType: " + SesMessageType);
 
@@ -32,14 +35,15 @@ exports.handler = function (event, context, callback) {
         SesDestinationAddress: { S: SesDestinationAddress },
         SesBounceSummary: { S: SesBounceSummary },
         SesMessageType: { S: SesMessageType },
+        ExpireEpoch: { N: ExpireEpoch },
       },
     };
     ddb.putItem(itemParams, function (err, data) {
       if (err) {
-        callback(err)
+        console.log("Error Saving Bounce: " + err);
+        callback(err);
       } else {
-        console.log(data);
-        callback(null,'')
+        callback(null, '');
       }
     });
   } else if (SesMessageType == "Delivery") {
@@ -53,14 +57,15 @@ exports.handler = function (event, context, callback) {
         SesReportingMTA: { S: SesReportingMTA1 },
         SesDestinationAddress: { S: SesDestinationAddress },
         SesMessageType: { S: SesMessageType },
+        ExpireEpoch: { N: ExpireEpoch },
       },
     };
     ddb.putItem(itemParamsdel, function (err, data) {
       if (err) {
-        callback(err)
+        console.log("Error Saving Delivery: " + err);
+        callback(err);
       } else {
-        console.log(data);
-        callback(null,'')
+        callback(null, '');
       }
     });
   } else if (SesMessageType == "Complaint") {
@@ -74,24 +79,20 @@ exports.handler = function (event, context, callback) {
         SesFeedbackId: { S: SesFeedbackId },
         SesDestinationAddress: { S: SesDestinationAddress },
         SesMessageType: { S: SesMessageType },
+        ExpireEpoch: { N: ExpireEpoch },
       },
     };
     ddb.putItem(itemParamscomp, function (err, data) {
       if (err) {
         console.log("Error Saving Complaint: " + err);
-        callback(err)
+        callback(err);
       } else {
-        console.log("Success Saving Complaint: " + err);
-        console.log(data);
-        callback(null, '')
+        callback(null, '');
       }
     });
   } else {
-      let msg = "Unsupported Message Type: " + SesMessageType;
+      let msg = `Unsupported Message Type: ${SesMessageType}`;
       console.log(msg);
-      callback(msg, '200')
+      callback(null, msg);
   }
 };
-
-//const start = Date.now(); 
-///*gives the number of milliseconds elapsed since January 1, 1970 00:00:00 UTC.*/
