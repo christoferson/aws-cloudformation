6919ae5ebf96dd44587484a946805e7136b760a1
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-08-04 01:04:56 +0900

    Cloudformation Lambda - Java

---
+++ lambda/basic-lambda-java/src/main/java/com/demo/lambda/RequestFunctionHandler.java
@@ -0,0 +1,22 @@
+package com.demo.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class RequestFunctionHandler implements RequestHandler<Map<String,String>, String> {
+
+    private static final Logger LOGGER = Logger.getLogger(RequestFunctionHandler.class.getName());
+
+    public String handleRequest(Map<String, String> values, Context context) {
+
+        LOGGER.info("Start: " + values);
+
+        return String.valueOf("000");
+        
+    }
+
+}
